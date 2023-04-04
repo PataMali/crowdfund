1b9be513c7ad9c7a47eaa68ce7768d8b3e15c7be
Kefa698 <kefaombongi07@gmail.com>
2023-04-04 16:44:37 +0300

    script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config({path: '.env'});
+require("dotenv").config({ path: ".env" });
+require("hardhat-deploy");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
+// Prints the Celo accounts associated with the mnemonic in .env
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -11,37 +11,33 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-const getEnv = (variable, optional = false) => {
-  if (!process.env[variable]) {
-    if (optional) {
-      console.warn(`[@env]: Environmental variable for ${variable} is not supplied.`)
-    } else {
-      throw new Error(`You must create an environment variable for ${variable}`)
-    }
-  }
-
-  return process.env[variable]?.replace(/\\n/gm, '\n')
-}
-
-// Your mnemomic key
- const MNEMONIC = getEnv("MNEMONIC")
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
- module.exports = {
-  solidity: "0.8.4",
+module.exports = {
+  defaultNetwork: "alfajores",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:7545",
+    },
     alfajores: {
+      gasPrice: 1500000000,
+      gas: 4100000,
       url: "https://alfajores-forno.celo-testnet.org",
       accounts: {
         mnemonic: process.env.MNEMONIC,
-        path: "m/44'/60'/0'/0",
+        path: "m/44'/52752'/0'/0",
       },
-      chainId: 44787,
+      //chainId: 44787
+    },
+    celo: {
+      url: "https://forno.celo.org",
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/52752'/0'/0",
+      },
+      chainId: 42220,
     },
   },
+  solidity: "0.8.0",
 };
